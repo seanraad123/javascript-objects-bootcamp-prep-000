a9edccbd5e71b34985de281b017d76bfa08ec943
Sean Conrad <seanconrad123@gmail.com>
2018-03-31 00:20:27 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -6,8 +6,17 @@
 var playlist = { "50cent": "oatmeal" };
 
 function updatePlaylist(Obj_playlist, key_artist_name, value_song_title){
-   var Obj_playlist = {key_artist_name: value_song_title};
+  Obj_playlist[key_artist_name] = value_song_title};
    return Obj_playlist
 }
 
 p(Obj_playlist)
+
+
+
+
+function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
+  obj[key] = value
+ 
+  return obj
+}
