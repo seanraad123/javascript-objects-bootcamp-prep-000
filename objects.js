99e830a69251333838767e6b7309bf8d52d87c3b
Sean Conrad <seanconrad123@gmail.com>
2018-03-31 00:31:16 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -11,6 +11,7 @@ function updatePlaylist(Obj_playlist, key_artist_name, value_song_title){
 
 function removeFromPlaylist(Obj_playlist, artist_name){
   delete Obj_playlist.artist_name
+  return Obj_playlist
 }
 
 p(removeFromPlaylist(playlist, "Kanye_West"))
