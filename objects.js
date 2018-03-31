87dc8952660020b87beb2bacbc8754d3101d96b3
Sean Conrad <seanconrad123@gmail.com>
2018-03-31 00:12:20 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,11 +2,4 @@
     console.log(value);
  }
 
-var meals = { breakfast: "oatmeal", lunch: "turkey sandwich", dinner: "steak and potatoes" };
- 
-// the `delete` operator returns `true` if it has successfully
-// deleted, `false` otherwise
-delete meals.dinner; // true
- 
-p(meals);
-// returns { breakfast: "oatmeal", lunch: "turkey sandwich" }
+
